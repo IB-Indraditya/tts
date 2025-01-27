0e211d963a523a97db81f43b1ef135f8bc066eb7
Indra <144indra7@gmail.com>
2025-01-27 22:44:03 +0530

    Trigger GitHub Pages rebuild

--- deploy.bat
+++ deploy.bat
@@ -5,6 +5,14 @@ git pull origin main
 git add .
 git commit --allow-empty -m "Trigger GitHub Pages rebuild"
 git push origin main
-echo wait for a moment
-timeout /t 10 >nul
+echo updating...
+timeout /t 5 >nul
+echo updating...
+timeout /t 5 >nul
+echo Modifying as per the commitment
+timeout /t 5 >nul
+echo Checking
+timeout /t 5 >nul
+echo Deploying
+timeout /t 5 >nul
 echo changes made succesfully
